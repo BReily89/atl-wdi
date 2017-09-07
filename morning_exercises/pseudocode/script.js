4fb4d9c58b37435e5aa00b692c4c2d98066b47b3
BReily89 <BrandonReily.br@gmail.com>
2017-09-07 09:31:16 -0400

    pushing homework

--- morning_exercises/pseudocode/script.js
+++ morning_exercises/pseudocode/script.js
@@ -3,27 +3,34 @@
 // Barrels O' RUM
 
 var barrels = function(small, large, total){
+  var smallBarrel = 60;
+  var largeBarrel = (825 - (2 * 60))/705;
+
+  return largeBarrel;
 };
+
+barrels = ()
+return largeBarrel;
 
 //*************************
 // Problem 2:
 // Sailing the Seas
 
-var shipFuelCost = function(fuelPrice, milesPerGallon){
+//var shipFuelCost = function(fuelPrice, milesPerGallon){
   //your code here
-};
+//};
 
 //*************************
 // Problem 3:
 // GROG
 
-var calcFruitJuice = function(a, b, c, d){
+//var calcFruitJuice = function(a, b, c, d){
   //your code here
-};
+//};
 
 //DO NOT EDIT BELOW THIS LINE//
-module.exports = {
-  barrels: barrels,
-  shipFuelCost: shipFuelCost,
-  calcFruitJuice: calcFruitJuice
-};
+//module.exports = {
+ // barrels: barrels,
+ // shipFuelCost: shipFuelCost,
+  //calcFruitJuice: calcFruitJuice
+//};
