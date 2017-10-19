2d1407c6413e998d2e643e2c9930eee98c2c3b38
BReily89 <BrandonReily.br@gmail.com>
2017-10-19 19:46:04 -0400

    fuck carmen sandiego

--- labs/sql/library_sql/basic_queries.sql
+++ labs/sql/library_sql/basic_queries.sql
@@ -3,26 +3,26 @@
 
 
 -- Get all information about all authors
-
+SELECT * FROM authors;
 -- Get just the name and birth year of all authors
-
+SELECT birth_year, name, id FROM authors;
 -- Get all authors born in the 20th century or later
-
--- Get all authors born in the USA
-
--- Get all books published in 1985
-
+SELECT * FROM authors WHERE birth_year > 1900;
+-- Get all authors born in the USA'
+ SELECT * FROM authors WHERE nationality LIKE 'United S%;
+-- Get all books published in 1985'
+SELECT * FROM books WHERE publication_date = 1985;
 -- Get all books published before 1989
-
+SELECT * FROM books WHERE publication_date > 1989;
 -- Get just the title of all books.
-
+SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
 
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT * FROM books WHERE title LIKE '%the%';
 -- Add yourself as an author
 
--- Add two books that you'd like to write (you can hard-code your id as the author id)
+-- Add two books that you''d like to write (you can hard-code your id as the author id)
 
 -- Update one of your books to have a new title
 
