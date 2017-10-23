9ab0aaa65ae5f4d69b94e2e3305660cf7d9ae24d
BReily89 <BrandonReily.br@gmail.com>
2017-10-23 17:23:52 -0400

    NilClass

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -2,6 +2,14 @@ challenge 1
 1. a = true
 2. b = false
 3. a = b
+4. a =! b 
 
 4. a =! b
-5. 
+challenge 2 
+1. p = nil
+2.puts.p # {"was here"} can't get it to render
+3.puts
+4 p.class = NilClass
+
+
+
