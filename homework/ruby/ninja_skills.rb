9444a654afe80f25369e779f9394113bd93705dc
BReily89 <BrandonReily.br@gmail.com>
2017-10-23 17:37:45 -0400

    string skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -7,12 +7,24 @@ challenge 1
 4. a =! b
 challenge 2 
 1. p = nil
-2.puts.p # {"was here"} can't get it to render
+2.puts.p # {"was here"} can't get it to render 
 3.puts
 4 p.class = NilClass
  Challenge 3
  1.d =4
  2. e = 2.4
  3. d = e = 2.4
+
+ Challenge 4;
+ 1.christine = "Hi I'm christine"
+ 2. likesTo = " long walks on the beach "
+ 3. [Christine +  "I like" likesTo]
+4. gToNumber = 2
+5. pry(main)> gToNumber = 2 it changes from a string to an integer
+6. TypeError: no implicit conversion of Integer into String
+    you can put the "gToNumber" in brackets and add it to g and it should return 2 + "2"
+
+    Challenge 5;
+1.
 
 
