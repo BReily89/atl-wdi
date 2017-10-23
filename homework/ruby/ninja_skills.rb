c07f81d05d2011592415c9a979b728c32b18aa30
BReily89 <BrandonReily.br@gmail.com>
2017-10-23 16:01:27 -0400

    boolean skills

---
+++ homework/ruby/ninja_skills.rb
@@ -0,0 +1,7 @@
+challenge 1
+1. a = true
+2. b = false
+3. a = b
+
+4. a =! b
+5. 
--- labs/sql/library_sql/advanced_queries.sql
+++ labs/sql/library_sql/advanced_queries.sql
@@ -12,9 +12,15 @@
 -- Find out how many books were written by US authors.
 
 -- Find all books written after 1930 by authors from Argentina.
-
+SELECT * FROM authors
+JOIN books ON authors.id = books.author_id
+WHERE authors.nationality = 'Argentina'
+AND books.publication_date = 1930;
 -- Find all books written before 1980 by authors not from the US.
-
+ SELECT * FROM books 
+ JOIN authors ON books.author.id
+ WHERE books.publication_date < 1980
+ AND authors.nationality = 'United States of America';
 -- Find all authors whose names start with 'J'.
 
 -- Find all books whose titles contain 'the'.
